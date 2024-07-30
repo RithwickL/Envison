544f5a01f20a92221def26dfb37125bdea7f2a07
Dorotapoko <samrita.khurana2007@gmail.com>
2024-07-30 22:12:59 +0530

    hey

---
+++ data.js
@@ -0,0 +1,38 @@
+var threads = [
+    {
+        title: "Thread 1",
+        Author: "Aaron",
+        date: date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "jack",
+                date: date.now(),
+                content: "hey there"
+            },
+            {
+                author: "Arthur",
+                date: date.now(),
+                content: "hey to you too"
+            }
+        ]
+    },
+    {
+        title: "Thread 2",
+        Author: "Aaron",
+        date: date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "jack",
+                date: date.now(),
+                content: "hey there"
+            },
+            {
+                author: "Arthur",
+                date: date.now(),
+                content: "hey to you too"
+            }
+        ]
+    }
+]
